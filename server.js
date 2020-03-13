7df3cabb64a8c325459c334966760995f8563ff8
James Helms <helms.james@gmail.com>
2020-03-13 13:59:58 -0500

    fixed routes

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
  });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+require("./routes/api.js")(app);
+require("./routes/view.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
